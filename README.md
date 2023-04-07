c0acd17b29a056070407c8eec36c1cc01215da61
AnnaSt <anutabatuk@gmail.com>
2023-04-07 14:21:07 +0800

    first version

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/cfcbx4tbxh5jx5qe?svg=true)](https://ci.appveyor.com/project/AnutaSt/ajs-hw-advanced)
---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+// TODO: write your code here
+import validateUsername from './nikname';
+
+// eslint-disable-next-line no-console
+console.log(validateUsername('gjkdls-gj_3jj4ln'));
+
